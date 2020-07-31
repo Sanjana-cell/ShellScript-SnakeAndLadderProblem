b62ae8bd85a50ef961ee45c2164752f80a36c6a5
Sanjana-cell <55678866+Sanjana-cell@users.noreply.github.com>
2020-08-01 00:19:06 +0530

    Refactor:  added comments and provided correct indentation

--- snakeGame.sh
+++ snakeGame.sh
@@ -9,15 +9,16 @@ position=0
 option=0
 
 echo "Welcome to Snake and Ladder"
-dice_number=$(($RANDOM%6+1))
+dice_number=$(($RANDOM%6+1)) #generates number from 1 to 6
 echo "Player got " $dice_number "by rolling the dice" 
-option=$(($RANDOM%3))
+option=$(($RANDOM%3)) #generates number from 0 to 2
+#switch case to match the option with any one of the case and change position 
 case $option in
-	$LADDER)	position=$(($position + $dice_number));;
+	$LADDER)position=$(($position + $dice_number));;
 	$NO_PLAY)position=$position;;
 	$SNAKE)	position=$(($position - $dice_number));;
 esac
-
+#checks if position is minus value if it is minus value then assigns with start position  
 if [ $position -lt 0 ]
 then
 		position=$START_POSITION
