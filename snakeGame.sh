bdfc08f805a91144f49e57e7362e8b872ac32ae0
Sanjana K R <sanjanakr97@gmail.com>
2020-08-01 00:55:44 +0530

    Added: games continues till player reaches winning position

--- snakeGame.sh
+++ snakeGame.sh
@@ -1,27 +1,30 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 SNAKE=0
 NO_PLAY=1
 LADDER=2
 START_POSITION=0
+WINING_POSITION=100
 
 position=0
 option=0
 
 echo "Welcome to Snake and Ladder"
-dice_number=$(($RANDOM%6+1)) #generates number from 1 to 6
-echo "Player got " $dice_number "by rolling the dice" 
-option=$(($RANDOM%3)) #generates number from 0 to 2
-#switch case to match the option with any one of the case and change position 
-case $option in
-	$LADDER)position=$(($position + $dice_number));;
-	$NO_PLAY)position=$position;;
-	$SNAKE)	position=$(($position - $dice_number));;
-esac
-#checks if position is minus value if it is minus value then assigns with start position  
-if [ $position -lt 0 ]
-then
+while [ $position -le $WINING_POSITION ]
+do
+	dice_number=$(($RANDOM%6+1)) #generates number from 1 to 6
+ 	option=$(($RANDOM%3)) #generates number from 0 to 2
+	#switch case to match the option with any one of the case and change position 
+	case $option in
+		$LADDER)position=$(($position + $dice_number));;
+		$NO_PLAY)position=$position;;
+		$SNAKE)	position=$(($position - $dice_number));;
+	esac
+	#checks if position is minus value if it is minus value then assigns with start position  
+	if [ $position -lt 0 ]
+	then
 		position=$START_POSITION
-fi
-
-echo "Player postion " $position 
+	fi
+	echo " Current postion " $position
+done
+echo "CONGRAGULATIONS !! YOU WON"
